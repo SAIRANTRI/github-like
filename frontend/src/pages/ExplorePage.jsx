cc1dcb723126d0d13d0181ded3298a793bd84ac2
SAIRANTRI <pathaksairantri@gmail.com>
2024-12-18 12:58:30 +0530

    Project completed(added liking and getLikes features)

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -13,7 +13,7 @@ const ExplorePage = () => {
 		setLoading(true);
 		setRepos([]);
 		try {
-			const res = await fetch("http://localhost:5000/api/explore/repos/" + language);
+			const res = await fetch("/api/explore/repos/" + language);
 			const { repos } = await res.json(); // `items` contains the repository data
 			setRepos(repos);
 			setSelectedLanguage(language);
